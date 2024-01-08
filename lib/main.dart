fc0c29f5d669b506dc78402e57ea5503edd94234
raufendro-dev <raufendro@gmail.com>
2024-01-08 10:01:00 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
